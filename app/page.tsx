d4425fdecd4195dc7ac5d1cbe65127896f6e3d54
Appdev <mehulkorat552@gmail.com>
2025-11-26 18:46:01 +0530

    Project Start Up

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+
+
+export default function Home() {
+  return (
+    <div className="main-body">
+      
+      <div className="card">
+        <div className="left">
+          <div className="parent-div" >
+            <div className="logo">PDF</div>
+            <div>
+              <h1>LongPDF</h1>
+              <p>Fast PDF reader & tools — open, read, and manage PDFs quickly.</p>
+            </div>
+          </div>
+          <div className="buttons" >
+            <a className="btn btn-email" href="mailto:info@dynzo.in">Email Support</a>
+            <a className="btn btn-download" href="https://dynzo.in.in/download">Download Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
